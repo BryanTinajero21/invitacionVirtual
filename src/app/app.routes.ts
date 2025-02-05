58548f80603a7e04ef4209dae31c773b64a1714c
Bryan Tinajero Vazquez <bryan.tinajero@imss.gob.mx>
2025-02-05 12:03:13 -0600

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PresentacionComponent } from './modules/presentacion/presentacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'presentación', pathMatch: 'full' },
+    { path: 'presentación', component: PresentacionComponent },
+];
+
